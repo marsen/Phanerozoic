e87785664c1e4c42a07bee29360cffe5589e4edd
Knight Huang <knighthuang@91app.com>
2019-12-11 23:37:17 +0800

    191211 - Create CoverageProcessor

---
+++ src/Phanerozoic.Core.Test/Services/CoverageProcessorTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Phanerozoic.Core.Test.Services
+{
+    public class CoverageProcessorTest
+    {
+        [Fact]
+        public void Test_Process_Flow()
+        {
+        }
+    }
+}
