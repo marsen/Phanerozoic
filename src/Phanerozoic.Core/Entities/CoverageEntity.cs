27ec0791a5c41f2cf318d0ccc73198e0aeb25c67
Knight Huang <knighthuang@91app.com>
2019-12-11 23:22:31 +0800

    191211 - Create Parser Interface

---
+++ src/Phanerozoic.Core/Entities/CoverageEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Phanerozoic.Core.Entities
+{
+    public class CoverageEntity
+    {
+        public string Class { get; set; }
+        public string Method { get; set; }
+        public int Coverage { get; set; }
+    }
+}
---
+++ src/Phanerozoic.Core/Services/IReportParser.cs
@@ -0,0 +1,10 @@
+﻿using Phanerozoic.Core.Entities;
+using System.Collections.Generic;
+
+namespace Phanerozoic.Core.Services
+{
+    internal interface IReportParser
+    {
+        List<CoverageEntity> Parser(ReportEntity reportEntity);
+    }
+}
