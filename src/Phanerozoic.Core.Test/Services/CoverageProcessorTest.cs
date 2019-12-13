34e72b86e8f70bb241a5cc98feea9ce65d1a1307
Knight Huang <knighthuang@91app.com>
2019-12-13 17:32:08 +0800

    191213 - Create Test Stub

--- src/Phanerozoic.Core.Test/Services/CoverageProcessorTest.cs
+++ src/Phanerozoic.Core.Test/Services/CoverageProcessorTest.cs
@@ -1,11 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 using Phanerozoic.Core.Entities;
+using Phanerozoic.Core.Helpers;
 using Phanerozoic.Core.Services;
+using System;
 using Xunit;
 
 namespace Phanerozoic.Core.Test.Services
 {
     public class CoverageProcessorTest
     {
+        private readonly IServiceProvider _subServiceProvider;
+        private readonly IFileHelper _subFileHelper;
+        private readonly IReportParser _subReportParser;
+
+        public CoverageProcessorTest()
+        {
+            this._subFileHelper = Substitute.For<IFileHelper>();
+            this._subReportParser = Substitute.For<IReportParser>();
+
+            this._subServiceProvider = Substitute.For<IServiceProvider>();
+            this._subServiceProvider.GetService<IFileHelper>().Returns(this._subFileHelper);
+            this._subServiceProvider.GetService<IReportParser>().Returns(this._subReportParser);
+        }
+
         [Fact]
         public void Test_Process_Flow()
         {
@@ -13,13 +31,19 @@ namespace Phanerozoic.Core.Test.Services
             {
                 FilePath = "report.json"
             };
-            var target = new CoverageProcessor();
 
-            //// act
+            var target = GetTarget();
+
+            ////// act
             target.Process(reportEntity);
 
             //// assert
             Assert.True(true);
+        }
+
+        private ICoverageProcessor GetTarget()
+        {
+            return new CoverageProcessor(this._subServiceProvider);
         }
     }
 }
