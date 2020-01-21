a3cd3fdb69ede10c9594e2135c423d8687293622
Knight Huang <knighthuang@91app.com>
2020-01-21 19:20:55 +0800

    200121 - 同步資料時允許調整內容

--- src/Phanerozoic.Duplication/Program.cs
+++ src/Phanerozoic.Duplication/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Phanerozoic.Core.Entities;
 using Phanerozoic.Core.Services;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Phanerozoic.Duplication
@@ -45,13 +47,59 @@ namespace Phanerozoic.Duplication
 
             Console.WriteLine($"Source Sheets ID: {sourceId}");
             Console.WriteLine($"Target Sheets ID: {targetId}");
+            var sheetName = "Coverage";
 
             //// Read
-            var sourceList = googleSheetsService.GetValues(sourceId, "Coverage!A2:F100");
+            var firstRow = 2;
+            var maxRow = 100;
+            var range = $"{sheetName}!A{firstRow}:G{maxRow}";
+            var sourceList = googleSheetsService.GetValues(sourceId, range);
+            var methodList = Program.SheetRangeToEntityList(sourceList);
 
             //// Write
-            var targetRange = $"Coverage!A2:F{sourceList.Count + 1}";
-            googleSheetsService.SetValue(targetId, targetRange, sourceList);
+            var targetRange = $"{sheetName}!A{firstRow}:G{sourceList.Count + 1}";
+            var targetList = Program.EntityListToSheetRange(methodList);
+            googleSheetsService.SetValue(targetId, targetRange, targetList);
+        }
+
+        private static List<MethodEntity> SheetRangeToEntityList(IList<IList<object>> sheetRange)
+        {
+            List<MethodEntity> methodList = new List<MethodEntity>();
+            if (sheetRange != null && sheetRange.Count > 0)
+            {
+                foreach (var row in sheetRange)
+                {
+                    var methodEntity = new MethodEntity
+                    {
+                        Repository = row[0].ToString().Trim(),
+                        Project = row[1].ToString().Trim(),
+                        Class = row[2].ToString().Trim(),
+                        Method = row[3].ToString().Trim(),
+                        Coverage = int.Parse(row[4].ToString()),
+                        Team = row[6].ToString().Trim(),
+                        RawData = row,
+                    };
+
+                    if (string.IsNullOrWhiteSpace(methodEntity.Method) == false)
+                    {
+                        methodList.Add(methodEntity);
+                    }
+                }
+            }
+
+            return methodList;
+        }
+
+        private static IList<IList<object>> EntityListToSheetRange(List<MethodEntity> methodList)
+        {
+            var rowList = new List<IList<object>>();
+            foreach (var method in methodList)
+            {
+                var row = method.RawData;
+                rowList.Add(row);
+            }
+
+            return rowList;
         }
     }
 }
