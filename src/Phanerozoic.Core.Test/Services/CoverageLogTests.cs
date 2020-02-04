96b84176579a8623ba47097cdef5440372607fde
Knight Huang <knighthuang@91app.com>
2020-02-04 23:26:05 +0800

    200204 - 每周一欄的涵蓋率值

--- src/Phanerozoic.Core.Test/Services/CoverageLogTests.cs
+++ src/Phanerozoic.Core.Test/Services/CoverageLogTests.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 using Phanerozoic.Core.Entities;
-using System;
-using System.Collections.Generic;
+using Phanerozoic.Core.Helpers;
 using Xunit;
 
 namespace Phanerozoic.Core.Services.Tests
@@ -10,36 +12,48 @@ namespace Phanerozoic.Core.Services.Tests
     public class CoverageLogTests
     {
         private readonly IGoogleSheetsService _stubGoogleSheetsService;
+        private readonly IDateTimeHelper _stubDateTimeHelper;
         private readonly IServiceProvider _stubServiceProvider;
+        private readonly IConfiguration _stubConfiguration;
 
         public CoverageLogTests()
         {
             this._stubGoogleSheetsService = Substitute.For<IGoogleSheetsService>();
+            this._stubConfiguration = Substitute.For<IConfiguration>();
+            this._stubDateTimeHelper = Substitute.For<IDateTimeHelper>();
 
             this._stubServiceProvider = Substitute.For<IServiceProvider>();
             this._stubServiceProvider.GetService<IGoogleSheetsService>().Returns(this._stubGoogleSheetsService);
+            this._stubServiceProvider.GetService<IDateTimeHelper>().Returns(this._stubDateTimeHelper);
         }
 
-        [Fact()]
+        [Fact(DisplayName = "每周一欄")]
         public void LogTest()
+        {
+            this.SheetRangeAssert(new DateTime(2019, 1, 1), "2019!A1");
+            this.SheetRangeAssert(new DateTime(2019, 12, 31), "2019!BA1");
+            this.SheetRangeAssert(new DateTime(2020, 1, 1), "2020!A1");
+        }
+
+        private CoverageLog GetTarget()
+        {
+            return new CoverageLog(this._stubServiceProvider, this._stubConfiguration);
+        }
+
+        private void SheetRangeAssert(DateTime time, string range)
         {
             //// Arrange
             var methodList = new List<MethodEntity>
             {
                 new MethodEntity()
             };
+            this._stubDateTimeHelper.Now.Returns(time);
 
             //// Act
-            var target = GetTarget();
-            target.Log(methodList);
+            GetTarget().Log(methodList);
 
             //// Assert
-            this._stubGoogleSheetsService.Received(1).SetValue(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<IList<IList<object>>>());
-        }
-
-        private CoverageLog GetTarget()
-        {
-            return new CoverageLog(this._stubServiceProvider);
+            this._stubGoogleSheetsService.Received(1).SetValue(Arg.Any<string>(), range, Arg.Any<IList<IList<object>>>());
         }
     }
 }
--- src/Phanerozoic.Core/Services/CoverageLog.cs
+++ src/Phanerozoic.Core/Services/CoverageLog.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Phanerozoic.Core.Entities;
-using Phanerozoic.Core.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Phanerozoic.Core.Entities;
+using Phanerozoic.Core.Helpers;
 
 namespace Phanerozoic.Core.Services
 {
@@ -11,26 +12,30 @@ namespace Phanerozoic.Core.Services
     {
         private readonly IDateTimeHelper _dateTimeHelper;
         private readonly IGoogleSheetsService _googleSheetsService;
+        private readonly IConfiguration _configuration;
+        private string _sheetsId;
 
-        public CoverageLog(IServiceProvider serviceProvider)
+        public CoverageLog(IServiceProvider serviceProvider, IConfiguration configuration)
         {
+            this._configuration = configuration;
             this._dateTimeHelper = serviceProvider.GetService<IDateTimeHelper>();
             this._googleSheetsService = serviceProvider.GetService<IGoogleSheetsService>();
+
+            this._sheetsId = this._configuration["Google:Sheets:Id"];
         }
 
         public void Log(IList<MethodEntity> methodList)
         {
-            //// C is 3
-            var firstColumn = 3;
+            var now = this._dateTimeHelper.Now;
+            int week = this.GetWeek(now);
+            string column = SheetHelper.ColumnToLetter(week);
+            var range = $"{now.Year}!{column}1";
 
-
-
-            this._googleSheetsService.SetValue(null, null, null);
+            this._googleSheetsService.SetValue(this._sheetsId, range, null);
         }
 
-        private int GetWeek()
+        private int GetWeek(DateTime now)
         {
-            var now = this._dateTimeHelper.Now;
             var week = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(now, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
             return week;
         }
